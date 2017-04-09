005e125627188b20dd846cc62b77620a5f5e0918
Callan Peter Milne <callan.milne@eviratec.com.au>
2017-04-10 04:06:56 +1000

    (fix):Removed unnec deps from Karma test config

--- src/js/devops/configs/karma.js
+++ src/js/devops/configs/karma.js
@@ -32,13 +32,7 @@ function initKarmaConfig ($config, grunt) {
       files: [
         'node_modules/angular/angular.js',
         'node_modules/angular-mocks/angular-mocks.js',
-        'node_modules/angular-material/angular-material.js',
-        'node_modules/angular-aria/angular-aria.js',
-        'node_modules/angular-animate/angular-animate.js',
-        'node_modules/angular-messages/angular-messages.js',
         'node_modules/angular-cookies/angular-cookies.js',
-        'node_modules/angular-ui-router/release/angular-ui-router.js',
-        'node_modules/angular-environment-config/release/angular-environment-config.js',
         'src/ng/module.es',
         'src/ng/*/**/*.es',
         'test/unit/**/*-spec.js',
